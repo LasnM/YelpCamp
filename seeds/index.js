6a14bc7c03f9540eec43690627df26e14864475d
MaLasn <lasnmargareth8@gmail.com>
2025-02-25 18:46:51 +0100

    db seeded

---
+++ seeds/index.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const seedHelpers = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+  await Campground.deleteMany({}); //delete everything in the database
+  for(let i = 0; i < 50; i++) {
+    const random1000 = Math.floor(Math.random() * 1000);
+    const camp = new Campground({
+      location: `${cities[random1000].city}, ${cities[random1000].state}`,
+      title: `${sample(seedHelpers.descriptors)} ${sample(seedHelpers.places)}`
+    });
+    await camp.save();
+  }
+}
+
+seedDB(); //run the function
