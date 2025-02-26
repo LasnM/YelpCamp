d4972bf714cb782f333b1ee9784f2917a64ea1c6
MaLasn <lasnmargareth8@gmail.com>
2025-02-26 17:16:28 +0100

    boilerplate base

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -18,6 +19,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.engine('ejs', ejsMate);
 
 app.get('/', (req, res) => {
   res.render('home');
