4dd63afe4c3f511547c98b533a777ba35970803a
MaLasn <lasnmargareth8@gmail.com>
2025-03-05 20:17:22 +0100

    review model

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const Joi = require('joi');
 const { campgroundSchema } = require('./schemas.js');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
