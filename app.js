95f569e6fe6cc3f4b28e3797465a9f1e17ad42f7
MaLasn <lasnmargareth8@gmail.com>
2025-03-06 15:26:43 +0100

    small fixes

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.engine('ejs', ejsMate);
 
 app.use('/campgrounds', campgrounds); //using campgrounds router for endpoint mapping
