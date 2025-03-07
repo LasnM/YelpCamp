e2e4c821d6c4b1c9f5818a5d11e064ba688c0060
MaLasn <lasnmargareth8@gmail.com>
2025-03-07 11:07:56 +0100

    cleaning up the code a little bit

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
 const Campground = require('./models/campground');
+const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema } = require('./schemas.js');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if(!req.isAuthenticated()) {
@@ -25,3 +27,13 @@ module.exports.isAuthor = async (req, res, next) => {
     }
     next();
 }
+
+module.exports.validateCampground = (req, res, next) => {
+    const {error} = campgroundSchema.validate(req.body);
+    if(error){
+      const msg = error.details.map(el => el.message).join(',');
+      throw new ExpressError(msg, 400);
+    } else {
+      next();
+    }
+  }
